d6ff910e7002e30539910daf37afe49f2398ddff
icytail <icytailovo@gmail.com>
2021-01-30 16:20:54 -0800

    fireball

--- client/game.js
+++ client/game.js
@@ -8,7 +8,7 @@ class playScenes extends Phaser.Scene
 	preload()
 	{
 		this.load.image('test-sprite', 'assets/test-sprite.png');
-		this.load.image('fireball', 'assets/fireball5.png');
+		this.load.image('fireball', 'assets/fireball.png');
 	}
 
     create()
@@ -20,8 +20,8 @@ class playScenes extends Phaser.Scene
 
 		this.otherPlayers = this.physics.add.group();
 
-		this.fireball = this.physics.add.sprite(400, 250, 'fireball');
-		
+		//this.fireball = this.physics.add.sprite(400, 250, 'fireball');
+
 		/* this.ball = this.add.circle(400, 250, 10, 0xffffff, 1);
 		this.physics.add.existing(this.ball);
 		this.ball.body.setBounce(1, 1);
@@ -35,16 +35,16 @@ class playScenes extends Phaser.Scene
 				if(player.playerID === self.socket.id){
 					self.addPlayer(self, player);
 				}
-				else 
+				else
 				{
-					self.addOtherPlayers(self, player);	
+					self.addOtherPlayers(self, player);
 				}
 			});
 		});
 
 		this.socket.on('playerMoved', function (playerInfo)
 		{
-			console.log(playerInfo);
+			//console.log(playerInfo);
 			self.otherPlayers.getChildren().forEach(function(otherPlayer){
 				if(playerInfo.playerID === otherPlayer.playerID)
 				{
@@ -68,6 +68,7 @@ class playScenes extends Phaser.Scene
 			self.playerType = 'finder';
 		}
 		self.player.setCollideWorldBounds(true);
+		self.physics.add.existing(self.player, true);
 	}
 
 	addOtherPlayers(self, playerInfo)
@@ -83,22 +84,46 @@ class playScenes extends Phaser.Scene
 		}
 		otherPlayer.playerID = playerInfo.playerID;
 		self.otherPlayers.add(otherPlayer);
+		console.log(self.otherPlayers.children);
+		let temp = self.otherPlayers.getChildren();
+		console.log(temp[0]);
+		for (let i=0;i<temp.length;i++) {
+			console.log(temp[i]);
+		}
 	}
-    
+
     update()
     {
 		let self = this;
-		if(this.player)
+		if (this.player)
 		{
 			this.updateMovement();
+			if (this.playerType != 'hider') {
+				this.emitFireBall();
+			}
 			this.updateServer();
 		}
+		if (this.fire) {
+			this.fire.body.setVelocityX(-this.velocity);
+			if (this.fire.body.collideWorldBounds) {
+				this.fire.destroy();
+			}
+			let temp = this.otherPlayers.getChildren();
+			let hidder = (temp[0]);
+			console.log(1);
+			this.physics.add.collider(this.fire, temp[0], ()=> {
+				console.log(this.fire);
+				this.fire.destroy();
+				this.fire = null;
+				hidder.destroy();
+			});
+		}
 	}
-	
+
 	updateMovement()
 	{
 		if(this.cursors.left.isDown)
-		{	
+		{
 			this.player.setVelocityX(-this.velocity);
 		}
 		else if (this.cursors.right.isDown)
@@ -121,6 +146,13 @@ class playScenes extends Phaser.Scene
 		else
 		{
 			this.player.setVelocityY(0);
+		}
+	}
+
+	emitFireBall() {
+		let spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+		if (Phaser.Input.Keyboard.JustDown(spaceBar)) {
+			this.fire = this.physics.add.sprite(this.player.x, this.player.y, 'fireball').setScale(0.05);
 		}
 	}
 
